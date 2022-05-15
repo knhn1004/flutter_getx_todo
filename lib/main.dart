7cf787b2b3a957fbf24acddd0695a82f165f5aa3
oliver <kunhan945@gmail.com>
2022-05-15 18:13:59 +0800

    constants get_storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_todo/app/modules/home/view.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() => runApp(TodoApp());
+void main() async {
+  await GetStorage.init();
+  runApp(TodoApp());
+}
 
 class TodoApp extends StatelessWidget {
   @override
