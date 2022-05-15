f7fdd2ca2e441a5a5526d4212a3a64b6ca634087
oliver <kunhan945@gmail.com>
2022-05-15 20:22:39 +0800

    add task in home controller

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -22,4 +22,12 @@ class HomeController extends GetxController {
   void changeChipIndex(int value) {
     chipIndex.value = value;
   }
+
+  bool addTask(Task task) {
+    if (tasks.contains(task)) {
+      return false;
+    }
+    tasks.add(task);
+    return true;
+  }
 }
--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -1,5 +1,7 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_getx_todo/app/data/models/task.dart';
 import 'package:flutter_getx_todo/app/modules/home/controller.dart';
 import 'package:flutter_getx_todo/app/widgets/icons.dart';
 import 'package:flutter_getx_todo/app/core/values/colors.dart';
@@ -21,68 +23,13 @@ class AddCard extends StatelessWidget {
       child: InkWell(
           onTap: () async {
             await Get.defaultDialog(
-              titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
-              radius: 5,
-              title: 'Task Type',
-              content: Form(
-                key: homeController.formKey,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
-                      child: TextFormField(
-                        controller: homeController.editController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Title',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return 'Please enter some text';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 3.0.wp, vertical: 5.0.wp),
-                      child: Wrap(
-                        spacing: 2.0.wp,
-                        runSpacing: 2.0.wp,
-                        children: icons
-                            .map((icon) => Obx(() {
-                                  final index = icons.indexOf(icon);
-                                  return ChoiceChip(
-                                    label: icon,
-                                    selected:
-                                        homeController.chipIndex.value == index,
-                                    onSelected: (bool selected) {
-                                      homeController.chipIndex.value =
-                                          selected ? index : 0;
-                                    },
-                                    selectedColor: Colors.grey[200],
-                                    backgroundColor: Colors.white,
-                                    pressElevation: 0,
-                                  );
-                                }))
-                            .toList(),
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: blue,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        minimumSize: const Size(150, 40),
-                      ),
-                      onPressed: () {},
-                      child: const Text('Confirm'),
-                    )
-                  ],
-                ),
-              ),
-            );
+                titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
+                radius: 5,
+                title: 'Task Type',
+                content:
+                    DialogForm(homeController: homeController, icons: icons));
+            homeController.editController.clear();
+            homeController.changeChipIndex(0);
           },
           child: DottedBorder(
               color: Colors.grey[400]!,
@@ -94,6 +41,135 @@ class AddCard extends StatelessWidget {
                   color: Colors.grey,
                 ),
               ))),
+    );
+  }
+}
+
+class DialogForm extends StatelessWidget {
+  const DialogForm({
+    Key? key,
+    required this.homeController,
+    required this.icons,
+  }) : super(key: key);
+
+  final HomeController homeController;
+  final List<Icon> icons;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: homeController.formKey,
+        child: Column(
+          children: [
+            InputArea(homeController: homeController),
+            ChipsArea(icons: icons, homeController: homeController),
+            SubmitButton(homeController: homeController, icons: icons)
+          ],
+        ));
+  }
+}
+
+class SubmitButton extends StatelessWidget {
+  const SubmitButton({
+    Key? key,
+    required this.homeController,
+    required this.icons,
+  }) : super(key: key);
+
+  final HomeController homeController;
+  final List<Icon> icons;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: blue,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        minimumSize: const Size(150, 40),
+      ),
+      onPressed: () {
+        if (homeController.formKey.currentState!.validate()) {
+          var currentIcon = icons[homeController.chipIndex.value];
+          int icon = currentIcon.icon!.codePoint;
+          String color = HexColor.toHex(currentIcon.color!);
+          Task task = Task(
+            title: homeController.editController.text,
+            icon: icon,
+            color: color,
+          );
+          Get.back();
+          homeController.addTask(task)
+              ? EasyLoading.showSuccess('Create success')
+              : EasyLoading.showError('Create failed');
+        }
+      },
+      child: const Text('Confirm'),
+    );
+  }
+}
+
+class ChipsArea extends StatelessWidget {
+  const ChipsArea({
+    Key? key,
+    required this.icons,
+    required this.homeController,
+  }) : super(key: key);
+
+  final List<Icon> icons;
+  final HomeController homeController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 3.0.wp, vertical: 5.0.wp),
+      child: Wrap(
+        spacing: 2.0.wp,
+        runSpacing: 2.0.wp,
+        children: icons
+            .map((icon) => Obx(() {
+                  final index = icons.indexOf(icon);
+                  return ChoiceChip(
+                    label: icon,
+                    selected: homeController.chipIndex.value == index,
+                    onSelected: (bool selected) {
+                      homeController.chipIndex.value = selected ? index : 0;
+                    },
+                    selectedColor: Colors.grey[200],
+                    backgroundColor: Colors.white,
+                    pressElevation: 0,
+                  );
+                }))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class InputArea extends StatelessWidget {
+  const InputArea({
+    Key? key,
+    required this.homeController,
+  }) : super(key: key);
+
+  final HomeController homeController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
+      child: TextFormField(
+        controller: homeController.editController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Title',
+        ),
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      ),
     );
   }
 }
